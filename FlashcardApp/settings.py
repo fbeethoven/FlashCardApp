4c1a85d94676c2ba387a2c1268b57c6368d79673
fbeethoven <fulgencio.beethoven@gmail.com>
2021-07-06 21:12:02 +0200

    debug = false

--- FlashcardApp/settings.py
+++ FlashcardApp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #        SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("DEBUG") == "True")
 
 ALLOWED_HOSTS = ['*']
 
