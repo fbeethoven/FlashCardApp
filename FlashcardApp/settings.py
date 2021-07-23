5bfb6eabe151eb8abfa8a21f84e436573daa42a5
fbeethoven <fulgencio.beethoven@gmail.com>
2021-07-23 23:35:11 +0200

    Static files

--- FlashcardApp/settings.py
+++ FlashcardApp/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(_file_))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
